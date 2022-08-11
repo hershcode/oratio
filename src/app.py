1c91c497ef18b2f34f72debadd3726f4a527d479
Harshinder Chadha <harshinderc@gmail.com>
2022-08-11 16:59:45 -0700

    Initial commit
    Flask app that allows interaction with model.

---
+++ src/app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request, redirect
+import os
+from werkzeug.utils import secure_filename
+from models.models import Oratio
+
+app = Flask(__name__)
+
+UPLOAD_FOLDER= "data/upload"
+app.config['SECRET_KEY'] = 'supersecretkey'
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+
+@app.route("/", methods=['GET', "POST"])
+def index():
+    if request.method == "POST":
+        print("Form Data received")
+
+        if "file" not in request.files:
+            return redirect(request.url)
+
+        file = request.files["file"]
+        if file.filename == "":
+            return redirect(request.url)
+        
+        if file:
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        
+        if file.filename in os.listdir(UPLOAD_FOLDER):
+            
+            model = Oratio()
+            word = model.predict('data/upload/'+file.filename)
+
+            return word
+
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True, threaded=True) 
